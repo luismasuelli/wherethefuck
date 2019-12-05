c421779fdeb17e9f5dd798f262c3cedbb5ddb253
luismasuelli <luis@triplemint.com>
2019-12-05 10:33:00 -0500

    Adding two extra base views

--- wtfapi/api/base_views.py
+++ wtfapi/api/base_views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class AuthenticatedAPIView(APIView):
@@ -8,3 +9,21 @@ class AuthenticatedAPIView(APIView):
     """
 
     authentication_classes = (TokenAuthentication, SessionAuthentication)
+
+
+class LoginRequiredAPIView(AuthenticatedAPIView):
+    """
+    This view, aside from authenticating via either token or session, requires a user to be
+      already logged in.
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+
+class LoginPartiallyRequiredAPIView(AuthenticatedAPIView):
+    """
+    This view, aside from authenticating via either token or session, requires a user to be
+      already logged in for non-GET/HEAD/OPTIONS operations.
+    """
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
